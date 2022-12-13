6100ffe65b023f4a2b0f7ec4979b2fc054887e77
Ian Skelskey <ianskelskey@gmail.com>
2022-12-12 21:38:11 -0500

    update jsdoc documentation jsdoc is still not working properly home page does not link to components and component documentation isnt right

--- pages/About.js
+++ pages/About.js
@@ -1,9 +1,14 @@
 import ContentPage from "../components/ContentPage";
 
+/**
+ * 
+ * @returns The about page.
+ */
 export default function About() {
     return (
-        <ContentPage pageComponent={
-            <h1>About the Photographer</h1>
-        } />
+        <ContentPage title='Your Name - About'
+            pageComponent={
+                <h1>About the Photographer</h1>
+            } />
     );
 }
